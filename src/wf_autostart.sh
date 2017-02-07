985c07806b892aacbf7dd29d9590a997e54cdaf9
RBEGamer <marcel.ochsendorf@gmail.com>
2017-02-07 23:06:14 +0100

    added cmake project

--- src/wf_autostart.sh
+++ src/wf_autostart.sh
@@ -2,10 +2,35 @@
 clear
 echo "----WF-STARTUP----"
 
+if [ "$(id -u)" != "0" ]; then
+echo "Please run with sudo !"
+exit 1
+fi
+
+cd wf_server
+
+
+echo "---- WF CMAKE----"
+cmake CMakeLists.txt
+
+echo "---- WF CMAKE CLEANUP----"
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm -R -f CMakeFiles
 
 
 echo "---- WF COMPILE----"
-cd wf_server
-sudo make all
-./wf_main.o
+make all
+
+
+clear
+echo "---- WF START----"
+./bin/wf_server
+
+
+
+
 cd ..
+
+
+
