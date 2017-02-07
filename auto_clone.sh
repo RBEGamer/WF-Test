be09583b25f0129fbff2bf4cee3c073ec28a4f56
RBEGamer <marcel.ochsendorf@gmail.com>
2017-02-07 23:14:04 +0100

    sudo fix

--- auto_clone.sh
+++ auto_clone.sh
@@ -2,6 +2,13 @@
 
 echo "---------------------------------"
 
+if [ "$(id -u)" != "0" ]; then
+echo "Please run with sudo !"
+exit 1
+fi
+
+
+
 cd ..
 git clone git@github.com:RBEGamer/WF-Test.git
 cd WF-Test
