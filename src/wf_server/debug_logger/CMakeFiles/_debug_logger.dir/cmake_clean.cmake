7eb4ac0c98b3f9529cb6088f327fb583fe7c88b0
RBEGamer <marcel.ochsendorf@gmail.com>
2017-03-01 01:51:04 +0100

    stuff ignore it

---
+++ src/wf_server/debug_logger/CMakeFiles/_debug_logger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_debug_logger.dir/debug_logger.cpp.o"
+  "lib_debug_logger.pdb"
+  "lib_debug_logger.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_debug_logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/wf_server/debug_logger/CMakeFiles/_debug_logger.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "lib_debug_logger.a"
+)
